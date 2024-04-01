d3dc70b239e3ae99c43d480d36599a6f86fe2033
Russell Yearwood <yearwoodrussel@gmail.com>
2024-04-01 14:52:15 -0700

    Add my solutions for recursion exercise

--- exercism_exercises/elixir/bird-count/lib/bird_count.ex
+++ exercism_exercises/elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,22 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  def today([]), do: nil
+  def today([head | _tail]), do: head
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  def increment_day_count([]), do: [1]
+  def increment_day_count([today | tail]), do: [today+1 | tail]
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
-  end
+  def has_day_without_birds?([today | _tail]) when today == 0, do: true
+  def has_day_without_birds?([_today | tail]), do: has_day_without_birds?(tail)
+  def has_day_without_birds?(_list), do: false
 
-  def total(list) do
-    # Please implement the total/1 function
-  end
+  def total([head| tail]), do: head + total(tail)
+  def total(_list), do: 0
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  # Verify 1st list item
+  def busy_days([head | tail]) when head >= 5, do: 1+busy_days(tail)
+  # Else pass rest of list as new list to check first item of
+  def busy_days([_head | tail]), do: busy_days(tail)
+  # if no list to pass, set value to 0
+  def busy_days(_list), do: 0
+
 end

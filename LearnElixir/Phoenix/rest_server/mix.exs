306ded87d118568e04d625e19039a4ecd6b9a10a
Russell Yearwood <yearwoodrussel@gmail.com>
2024-04-10 17:45:07 -0700

    Add cowboy missing dependecy

--- LearnElixir/Phoenix/rest_server/mix.exs
+++ LearnElixir/Phoenix/rest_server/mix.exs
@@ -38,7 +38,8 @@ defmodule RestServerApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:cowboy, "~> 2.0"}
     ]
   end
 

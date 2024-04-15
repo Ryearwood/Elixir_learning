3c4d8da5b0b0aed931d66493806270ed3adef63b
Russell Yearwood <yearwoodrussel@gmail.com>
2024-04-15 14:54:53 -0700

    Add GraphiQL capabilities, Schema and router info

--- LearnElixir/Phoenix/graphql_server/lib/graphql_server_web/router.ex
+++ LearnElixir/Phoenix/graphql_server/lib/graphql_server_web/router.ex
@@ -2,11 +2,18 @@ defmodule GraphqlServerWeb.Router do
   use GraphqlServerWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/api", GraphqlServerWeb do
-    pipe_through :api
+  scope "/" do
+    pipe_through(:api)
+
+    forward("/graphql", Absinthe.Plug, schema: GraphqlServerWeb.Schema)
+
+    forward("/graphiql", Absinthe.Plug.GraphiQL,
+      schema: GraphqlServerWeb.Schema,
+      interface: :playground
+    )
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
@@ -19,10 +26,10 @@ defmodule GraphqlServerWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
+      pipe_through([:fetch_session, :protect_from_forgery])
 
-      live_dashboard "/dashboard", metrics: GraphqlServerWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: GraphqlServerWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
--- LearnElixir/Phoenix/graphql_server/mix.exs
+++ LearnElixir/Phoenix/graphql_server/mix.exs
@@ -41,7 +41,9 @@ defmodule GraphqlServer.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:absinthe, "~> 1.7.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 

123f0f488be2663913846d45076cfde688104ee7
Russell Yearwood <yearwoodrussel@gmail.com>
2024-05-02 01:51:11 -0700

    Add completed solution for DateTime exercise "Library Fees"

--- exercism_exercises/elixir/Dates_and_Times/library-fees/lib/library_fees.ex
+++ exercism_exercises/elixir/Dates_and_Times/library-fees/lib/library_fees.ex
@@ -1,25 +1,50 @@
 defmodule LibraryFees do
-  def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
-  end
+
+  @monday 1
+  @day_in_seconds 24 * 60 * 60
+
+  def datetime_from_string(string), do: NaiveDateTime.from_iso8601!(string)
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    datetime
+    |> NaiveDateTime.to_time()
+    |> Time.before?(~T[12:00:00])
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    days_to_return = if before_noon?(checkout_datetime), do: 28 * @day_in_seconds, else: 29 * @day_in_seconds
+    NaiveDateTime.add(checkout_datetime, days_to_return)
+    |> NaiveDateTime.to_date()
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    actual_return_datetime
+    |> NaiveDateTime.to_date()
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    datetime
+    |> NaiveDateTime.to_date()
+    |> Date.day_of_week()
+    |> Kernel.==(@monday)
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    return_datetime = datetime_from_string(return)
+    late_fee =
+      checkout
+      |> datetime_from_string()
+      |> return_date()
+      |> days_late(return_datetime)
+      |> Kernel.*(rate)
+
+    if monday?(return_datetime) do
+      floor(late_fee * 0.50)
+    else
+      late_fee
+    end
+
   end
 end

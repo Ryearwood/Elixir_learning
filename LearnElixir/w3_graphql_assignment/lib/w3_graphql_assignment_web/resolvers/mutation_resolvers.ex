1f80dc16dcb8eef53122625527c89fb24646b5e0
Russell Yearwood <yearwoodrussel@gmail.com>
2024-05-29 13:09:13 -0700

    Remove With checks within resolvers, best practice

--- LearnElixir/w3_graphql_assignment/lib/w3_graphql_assignment_web/resolvers/mutation_resolvers.ex
+++ LearnElixir/w3_graphql_assignment/lib/w3_graphql_assignment_web/resolvers/mutation_resolvers.ex
@@ -4,27 +4,17 @@ Root of Absinthe Resolvers for Mutations
 """
   alias W3GraphqlAssignmentWeb.Resolvers.UserResolvers
 
-  # Helper Function for update function
-  def update_params(id, params) do
-    with {:ok, user} <- UserResolvers.get_user_by_id(%{id: id}, params) do
-      {:ok, Map.merge(user, params)}
-    else
-      {:error, _} -> {:error, params}
-    end
-  end
 
   # Resolver - updateUser Mutation
   def update_user(%{id: id} = params, _) do
-    with {:ok, updated_user_params} <- update_params(id, Map.delete(params, :id)) do
-      {:ok, updated_user_params}
-    else
-      {:error, _} -> {:error, params}
-    end
+    user = UserResolvers.get_user_by_id(%{id: id}, params) |> elem(1)
+    updated_params = Map.delete(params, :id)
+    {:ok, Map.merge(user, updated_params)}
   end
 
   # Resolver - createUser Mutation
-  # TODO - Complete this functionality once Schema confusion sorted out
+  # TODO - Complete this functionality
   def create_user(args, _) do
-    
+
   end
 end
